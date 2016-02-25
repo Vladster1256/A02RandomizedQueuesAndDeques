4c83d54417e20d28969f68ae90224fa3867d6cda
Vladlen <vcmariorocks@gmail.com>
2016-02-25 12:28:39 -0700

    Randomized Queue works now
    gg no re. All thats left to do is document the code, and create the
    client program

--- src/a02/Deque.java
+++ src/a02/Deque.java
@@ -7,155 +7,165 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class Deque<Item> implements Iterable<Item>
 {
-	private int size;         // number of elements on queue
-    private Node front;    // beginning of queue
-    private Node back; 	   // end of queue
-	
-	private class Node 
+	private int size; // number of elements on queue
+	private Node<Item> front; // beginning of queue
+	private Node<Item> back; // end of queue
+
+	private static class Node<Item>
 	{
 		private Item item;
-		private Node next;
-		private Node previous;
-    }
-	
+		private Node<Item> next;
+		private Node<Item> previous;
+	}
+
 	public Deque()
 	{
 		front = null;
 		back = null;
 		size = 0;
 	}
-	
+
 	public boolean isEmpty()
 	{
-		if(front == null)
+		if (front == null)
 			return true;
 		else
 			return false;
 	}
-	
+
 	public int size()
 	{
 		return size;
 
 	}
-	
+
 	public void addFirst(Item item)
 	{
-		Node oldfront = front;
-		front = new Node();
-		front.item = item;
-		front.next = oldfront;
-		front.previous = null;
-		if(isEmpty())
-		{
-			front = back;
-		}
+		if (item == null)
+			throw new NullPointerException("Don't insert Nulls dude");
 		else
 		{
+			Node<Item> oldfront = front;
+			front = new Node<Item>();
+			front.item = item;
 			front.next = oldfront;
+			front.previous = null;
+			if (isEmpty())
+			{
+				front = back;
+			} else
+			{
+				front.next = oldfront;
+			}
+			size++;
+			assert verify();
 		}
-		size++;
-		assert verify();
 	}
-	
+
 	public void addLast(Item item)
 	{
-		Node oldback = back;
-		back = new Node();
-		back.item = item;
-		back.next = null;
-		back.previous = oldback;
-		if(isEmpty())
-			front = back;
+		if (item == null)
+			throw new NullPointerException("Don't insert Nulls dude");
 		else
 		{
-			oldback.next = back;
+			Node<Item> oldback = back;
+			back = new Node<Item>();
+			back.item = item;
+			back.next = null;
+			back.previous = oldback;
+			if (isEmpty())
+				front = back;
+			else
+			{
+				oldback.next = back;
+			}
+			size++;
+			assert verify();
 		}
-		size++;
-		assert verify();
 	}
-	
+
 	public Item removeFirst()
 	{
-		if(isEmpty())
+		if (isEmpty())
 			throw new NoSuchElementException("Queue underflow");
 		Item item = front.item;
 		front = front.next;
 		size--;
-		if(isEmpty())
+		if (isEmpty())
 			back = null;
 		assert verify();
 		return item;
 	}
-	
+
 	public Item removeLast()
 	{
-		if(isEmpty())
+		if (isEmpty())
 			throw new NoSuchElementException("Queue underflow");
 		Item item = back.item;
 		back = back.previous;
 		size--;
-		if(isEmpty())
+		if (isEmpty())
 			back = null;
 		assert verify();
 		return item;
-		
+
 	}
-	
+
 	private boolean verify()
 	{
-		if(size < 0)
+		if (size < 0)
 		{
 			return false;
 		}
-		
-		else if(size == 0)
+
+		else if (size == 0)
 		{
-			if(front != null)
+			if (front != null)
 				return false;
-			if(back != null)
+			if (back != null)
 				return false;
 		}
-		
-		else if(size == 1)
+
+		else if (size == 1)
 		{
-			if(front == null || back == null)
+			if (front == null || back == null)
 				return false;
-			if(front != back)
+			if (front != back)
 				return false;
-			if(front.next != null)
+			if (front.next != null)
 				return false;
 		}
-		
+
 		else
 		{
-			if(front == null || back == null)
+			if (front == null || back == null)
 				return false;
-			if(front == back)
+			if (front == back)
 				return false;
-			if(front.next == null)
+			if (front.next == null)
 				return false;
-			if(back.next != null)
+			if (back.next != null)
 				return false;
-			
-			//verify that the size is equal to the number of actual nodes that exist.
+
+			// verify that the size is equal to the number of actual nodes that
+			// exist.
 			int nodeCount = 0;
-			Node checker = front;
-			for(int i = 0; i < size; i++)
+			Node<Item> checker = front;
+			for (int i = 0; i < size; i++)
 			{
-				if(checker != null && nodeCount <= size)
+				if (checker != null && nodeCount <= size)
 				{
 					nodeCount++;
 					checker = checker.next;
 				}
 			}
-			
-			Node backChecker = front;
-			while(backChecker.next != null)
+
+			Node<Item> backChecker = front;
+			while (backChecker.next != null)
 			{
 				backChecker = backChecker.next;
 			}
-			if(back != backChecker)
+			if (back != backChecker)
 				return false;
 		}
 		return true;
@@ -166,50 +176,50 @@ public class Deque<Item> implements Iterable<Item>
 	{
 		return new ItemIterator();
 	}
-	
+
 	private class ItemIterator implements Iterator<Item>
 	{
-		private Node current = front;
+		private Node<Item> current = front;
 
 		public boolean hasNext()
 		{
-			if(current != null)
+			if (current != null)
 				return true;
 			else
 				return false;
 		}
-		
-		public void remove()      
-		{ 
-			throw new UnsupportedOperationException();  
+
+		public void remove()
+		{
+			throw new UnsupportedOperationException();
 		}
 
 		public Item next()
 		{
-			if (!hasNext()) throw new NoSuchElementException();
-            Item item = current.item;
-            current = current.next; 
-            return item;
+			if (!hasNext())
+				throw new NoSuchElementException();
+			Item item = current.item;
+			current = current.next;
+			return item;
 		}
-		
+
 	}
-	
 
 	public static void main(String[] args)
 	{
-		 Deque something = new Deque();
-		 something.addLast("a ");
-		 something.addLast("b ");
-		 something.addFirst("c ");
-		 something.addFirst("d ");
-		 String temp = (String) something.removeLast();
-		 StdOut.print(temp);
-		 temp = (String) something.removeFirst();
-		 StdOut.print(temp);
-		 temp = (String) something.removeLast();
-		 StdOut.print(temp);
-		 temp = (String) something.removeFirst();
-		 StdOut.print(temp);
+		Deque something = new Deque();
+		something.addLast(1);
+		something.addLast(2);
+		something.addFirst(3);
+		something.addFirst(4);
+		int temp = (int) something.removeLast();
+		StdOut.print(temp);
+		temp = (int) something.removeFirst();
+		StdOut.print(temp);
+		temp = (int) something.removeLast();
+		StdOut.print(temp);
+		temp = (int) something.removeFirst();
+		StdOut.print(temp);
 	}
 
 }
--- src/a02/RandomizedQueue.java
+++ src/a02/RandomizedQueue.java
@@ -1,38 +1,153 @@
 package a02;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomizedQueue<Item> implements Iterable<Item>
 {
-		public RandomizedQueue() 
-		{
+	private Item[] queue;
+	private int last;
+	private int size;
 
-		}
-		public boolean isEmpty()  
-		{
+	public RandomizedQueue()
+	{
+		queue = (Item[]) new Object[2];
+		size = 0;
+		last = 0;
+	}
+
+	public boolean isEmpty()
+	{
+		if (size == 0)
+			return true;
+		else
 			return false;
-		}
-		public int size()         
+	}
+
+	public int size()
+	{
+		return size;
+	}
+
+	private void resize(int capacity)
+	{
+		assert capacity >= size;
+		Item[] temp = (Item[]) new Object[capacity];
+		for (int i = 0; i < size; i++)
 		{
-			return 0;
+			temp[i] = queue[i];
 		}
-		public void enqueue(Item item)
+		queue = temp;
+	}
+
+	public void enqueue(Item item)
+	{
+		if (item == null)
+			throw new NullPointerException("Don't insert Nulls dude");
+		else
 		{
+			if (size == queue.length)
+				resize(2 * queue.length);
+			last = size;
+			queue[size++] = item;
 		}
-		public Item dequeue()  
+	}
+
+	public Item dequeue()
+	{
+		if (isEmpty())
+			throw new NoSuchElementException("Stack underflow");
+		int randomInt = StdRandom.uniform(size);
+		Item item = queue[randomInt];
+		if (randomInt == last)
 		{
-			return null;
-		}
-		public Item sample()           
+			queue[last] = null;
+			last--;
+			size--;
+		} else
 		{
-			return null;
+			queue[randomInt] = queue[last];
+			queue[last] = null;
+			last--;
+			size--;
 		}
-		public Iterator<Item> iterator()      
+
+		if (size > 0 && size == queue.length / 4)
+			resize(queue.length / 2);
+		return item;
+
+	}
+
+	public Item sample()
+	{
+		if (isEmpty())
+			throw new NoSuchElementException("Stack underflow");
+		int randomInt = StdRandom.uniform(size);
+		return queue[randomInt];
+	}
+
+	public Iterator<Item> iterator()
+	{
+		return new QueueIterator();
+	}
+
+	private class QueueIterator implements Iterator<Item>
+	{
+		private int i;
+
+		public QueueIterator()
 		{
-			return null;
+			i = size - 1;
 		}
-		public static void main(String[] args) 
+
+		@Override
+		public boolean hasNext()
 		{
-			
+			return i >= 0;
 		}
+
+		public void remove()
+		{
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public Item next()
+		{
+			if (!hasNext())
+				throw new NoSuchElementException();
+			return queue[i--];
+		}
+
+	}
+
+	public static void main(String[] args)
+	{
+		/**
+		 * Here is some code to test if the randomized queue works How this test
+		 * works is we fill the queue with strings that are just ints converted
+		 * to string, and when we dequeue, we can see if we return null because
+		 * string is an object and we can see if re are returning nulls. (It did
+		 * for a while until it was fixed)
+		 */
+		RandomizedQueue something = new RandomizedQueue();
+		for (int i = 0; i < 40; i++)
+		{
+			something.enqueue(Integer.toString(i));
+		}
+
+		for (int x = 0; x < 40; x++)
+		{
+			StdOut.print(something.queue[x] + " ");
+		}
+		StdOut.println("Here is the random dequeued stuff");
+		for (int j = 0; j < 10; j++)
+		{
+			String temp = (String) something.dequeue();
+			StdOut.print(temp + " ");
+		}
+	}
 }
